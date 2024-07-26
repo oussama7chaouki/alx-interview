2c3e0f25d391e486f12032f5fd9cad20c74d2778
oussama chaouki pc <oussama11chaouki@gmail.com>
2024-07-26 01:28:16 +0100

    fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""" determine the fewest number of coins needed to meet a given amount total """
+""" determine the fewest number of coins needed to meet given amount total """
 
 
 def makeChange(coins, total):
